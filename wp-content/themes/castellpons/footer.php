3405d5eacbef760f40052137440545fee441e4cb
MatiRosero <gekidasa@gmail.com>
2018-05-02 23:29:59 -0600

    footer columns

--- wp-content/themes/castellpons/footer.php
+++ wp-content/themes/castellpons/footer.php
@@ -1,37 +1,45 @@
 <?php
 /**
- * The template for displaying the footer. 
+ * The template for displaying the footer.
  *
  * Comtains closing divs for header.php.
  *
  * For more info: https://developer.wordpress.org/themes/basics/template-files/#template-partials
- */			
+ */
  ?>
-					
-				<footer class="footer" role="contentinfo">
-					
+
+				<footer id="site-footer" class="footer" role="contentinfo">
+
 					<div class="inner-footer grid-x grid-margin-x grid-padding-x">
-						
-						<div class="small-12 medium-12 large-12 cell">
+
+						<div id="footer-social-menu" class="small-12 medium-4 large-4 cell">
+							SOCIAL
 							<nav role="navigation">
 	    						<?php joints_footer_links(); ?>
 	    					</nav>
 	    				</div>
-						
-						<div class="small-12 medium-12 large-12 cell">
+
+						<div id="footer-credits" class="small-12 medium-4 large-4 cell">
 							<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
 						</div>
-					
+
+						<div id="footer-language-menu" class="small-12 medium-4 large-4 cell">
+							LANGUAGE
+							<nav role="navigation">
+	    						<?php joints_footer_links(); ?>
+	    					</nav>
+	    				</div>
+
 					</div> <!-- end #inner-footer -->
-				
+
 				</footer> <!-- end .footer -->
-			
+
 			</div>  <!-- end .off-canvas-content -->
-					
+
 		</div> <!-- end .off-canvas-wrapper -->
-		
+
 		<?php wp_footer(); ?>
-		
+
 	</body>
-	
+
 </html> <!-- end page -->

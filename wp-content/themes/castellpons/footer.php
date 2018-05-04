d9e8c7e770dae5ce6d3cdfa3ebdadf38bcb1ae97
MatiRosero <gekidasa@gmail.com>
2018-05-04 17:01:24 -0600

    footer style

--- wp-content/themes/castellpons/footer.php
+++ wp-content/themes/castellpons/footer.php
@@ -19,7 +19,8 @@
 	    				</div>
 
 						<div id="footer-credits" class="small-12 medium-4 large-4 cell">
-							<!-- <p class="source-org copyright">&copy; <?php echo date('Y'); ?> --><?php bloginfo('name'); ?>.</p>
+							<!-- <p class="source-org copyright">&copy; <?php echo date('Y'); ?> --><?php //bloginfo('name'); ?><strong>Castell + Pons</strong> Arquitectes</p>
+							
 						</div>
 
 						<?php if ( is_active_sidebar( 'footer-right' ) ) : ?>

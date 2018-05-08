27175a54bcd58597eaa996915122fad6a34cf97c
MatiRosero <gekidasa@gmail.com>
2018-05-07 20:34:08 -0600

    archive for projects

---
+++ wp-content/themes/castellpons/archive-cp-project.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Displays archive pages if a speicifc template is not set.
+ *
+ * For more info: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ */
+
+get_header(); ?>
+
+	<div class="content">
+
+		<div class="inner-content">
+
+		    <main class="main news-content" role="main">
+
+		    	<header>
+		    		<h1 class="page-title"><?php the_archive_title();?></h1>
+					<?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
+		    	</header>
+
+		    	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+					<!-- To see additional archive styles, visit the /parts directory -->
+					<?php get_template_part( 'parts/loop', 'archive-grid' ); ?>
+
+				<?php endwhile; ?>
+
+					<?php joints_page_navi(); ?>
+
+				<?php else : ?>
+
+					<?php get_template_part( 'parts/content', 'missing' ); ?>
+
+				<?php endif; ?>
+
+			</main> <!-- end #main -->
+
+	    </div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
--- wp-content/themes/castellpons/parts/loop-archive-grid.php
+++ wp-content/themes/castellpons/parts/loop-archive-grid.php
@@ -15,7 +15,7 @@ $grid_columns = 4; ?>
 <?php endif; ?> 
 
 		<!--Item: -->
-		<div class="small-6 medium-3 large-3 cell panel" data-equalizer-watch>
+		<div class="small-2 medium-3 large-3 cell panel" data-equalizer-watch>
 		
 			<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">
 			

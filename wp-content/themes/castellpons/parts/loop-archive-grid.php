748d9360d37ed38ad81c2a8f403450ad3416a54b
MatiRosero <gekidasa@gmail.com>
2018-05-07 20:40:45 -0600

    no data equalizer for archive grid

--- wp-content/themes/castellpons/parts/loop-archive-grid.php
+++ wp-content/themes/castellpons/parts/loop-archive-grid.php
@@ -10,35 +10,30 @@ $grid_columns = 4; ?>
 
 <?php if( 0 === ( $wp_query->current_post  )  % $grid_columns ): ?>
 
-    <div class="grid-x grid-margin-x grid-padding-x archive-grid" data-equalizer> <!--Begin Grid--> 
+    <div class="grid-x grid-margin-x grid-padding-x archive-grid" > <!--Begin Grid-->
 
-<?php endif; ?> 
+<?php endif; ?>
 
 		<!--Item: -->
-		<div class="small-2 medium-3 large-3 cell panel" data-equalizer-watch>
-		
+		<div class="small-2 medium-3 large-3 cell panel">
+
 			<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">
-			
+
 				<section class="featured-image" itemprop="articleBody">
 					<?php the_post_thumbnail('full'); ?>
 				</section> <!-- end article section -->
-			
+
 				<header class="article-header">
-					<h3 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>	
-					<?php get_template_part( 'parts/content', 'byline' ); ?>				
-				</header> <!-- end article header -->	
-								
-				<section class="entry-content" itemprop="articleBody">
-					<?php the_content('<button class="tiny">' . __( 'Read more...', 'jointswp' ) . '</button>'); ?> 
-				</section> <!-- end article section -->
-								    							
+					<h3 class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+				</header> <!-- end article header -->
+
 			</article> <!-- end article -->
-			
+
 		</div>
 
 <?php if( 0 === ( $wp_query->current_post + 1 )  % $grid_columns ||  ( $wp_query->current_post + 1 ) ===  $wp_query->post_count ): ?>
 
-   </div>  <!--End Grid --> 
+   </div>  <!--End Grid -->
 
 <?php endif; ?>
 

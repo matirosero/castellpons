c76165a11f978b08517fed65b3b70ffa72086ff5
MatiRosero <gekidasa@gmail.com>
2018-05-16 20:53:34 -0600

    touch for footer

--- wp-content/themes/castellpons/assets/scripts/drop-up-side.js
+++ wp-content/themes/castellpons/assets/scripts/drop-up-side.js
@@ -3,20 +3,19 @@
  */
 
 jQuery(document).ready(function($) {
-	
+
 	var trigger = $('.drop-up-side .cp-current-language > .wpml-ls-link'),
 		menu = $('.drop-up-side');
-	
-	trigger.on('mouseenter', function(event){
+
+	trigger.on('mouseenter touchstart', function(event){
 		console.log('hover');
 		$(this).next('.submenu').addClass('active');
 	});
 
-	menu.on('mouseleave', function(event){
+	menu.on('mouseleave touchend', function(event){
 		console.log('leave');
 		if ( $(this).find('.submenu').hasClass('active') ) {
 			$(this).find('.submenu').removeClass('active');
 		}
-		
 	});
 });

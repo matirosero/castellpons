9462affa927b5d927b57a75c32dd0de68e8f40d5
MatiRosero <gekidasa@gmail.com>
2018-05-16 22:05:36 -0600

    offices shortcode

--- wp-content/themes/castellpons/functions/cp-shortcodes.php
+++ wp-content/themes/castellpons/functions/cp-shortcodes.php
@@ -6,10 +6,76 @@
  * @return null
  */
 function ct_register_shortcodes() {
-    add_shortcode('caja-texto', 'ct_callout_box');
+    add_shortcode('caja-texto', 'ct_callout_box_shortcode');
+    add_shortcode('oficinas', 'ct_offices_shortcode');
 }
 add_action( 'init', 'ct_register_shortcodes' );
 
-function ct_callout_box( $atts, $content = null ) {
+function ct_callout_box_shortcode( $atts, $content = null ) {
 	return '<div class="callout">' . $content . '</div>';
+}
+
+function ct_offices_shortcode( $atts ) {
+	global $wp_query,
+        $post;
+
+    $return = '';
+
+	/*
+	 * The WordPress Query class.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/WP_Query
+	 */
+	$args = array(
+
+		// Type & Status Parameters
+		'post_type'   => 'cp-office',
+		'post_status' => 'publish',
+
+		// Order & Orderby Parameters
+		'order'               => 'ASC',
+		'orderby'             => 'date',
+
+		// Pagination Parameters
+		'posts_per_page'         => -1,
+
+		// Parameters relating to caching
+		'no_found_rows'          => false,
+		'cache_results'          => true,
+		'update_post_term_cache' => true,
+		'update_post_meta_cache' => true,
+
+	);
+    
+    $query = new WP_Query( $args );
+
+	if( ! $query->have_posts() ) :
+	    return false;
+	else : 
+
+		$return .= '<ul class="offices">';
+
+
+		while( $query->have_posts() ) : $query->the_post();
+
+	        $return .= '<li class="location">
+	        	<h3 class="location-city">'.get_the_title().'</h3>
+	        	<p class="location-address">'.wpautop( get_the_content() ).'</p>
+	        	<div class="location-map">';
+
+	        $return .= do_shortcode( get_post_meta( get_the_ID(), 'mro_cp_office_map', true ) );
+
+			$return .= '</div>
+	        	</li>';
+
+	    endwhile;
+
+	    $return .=  '</ul>';
+
+	    wp_reset_postdata();
+
+	endif;
+    
+	
+	return $return;
 }

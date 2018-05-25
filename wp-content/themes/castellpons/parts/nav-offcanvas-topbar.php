552947d181209cd46ab3bf793712cb23b7f84b2b
MatiRosero <gekidasa@gmail.com>
2018-05-24 19:51:16 -0600

    sticky works for desktop

--- wp-content/themes/castellpons/parts/nav-offcanvas-topbar.php
+++ wp-content/themes/castellpons/parts/nav-offcanvas-topbar.php
@@ -5,8 +5,34 @@
  * For more info: http://jointswp.com/docs/off-canvas-menu/
  */
 ?>
-
-<div class="top-bar" id="top-bar-menu">
+<div data-sticky-container>
+  <div class="top-bar" id="top-bar-menu" data-sticky data-options="marginTop:0;" style="width:100%">
+    <div class="title-area top-bar-left float-left">
+		<ul class="menu">
+			<li>
+				<?php
+				if ( function_exists( 'the_custom_logo' ) ) {
+					the_custom_logo();
+				} else { ?>
+					<a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+				<?php } ?>
+			</li>
+		</ul>
+    </div>
+    <div class="top-bar-right show-for-medium">
+    	<?php joints_top_nav(); ?>
+    </div>
+	<div class="top-bar-right float-right show-for-small-only">
+		<ul class="menu">
+			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
+			<li><a data-toggle="off-canvas"><i class="icon-menu-toggle"></i></a></li>
+		</ul>
+	</div>
+  </div>
+</div>
+<?php /*
+<div data-sticky-container>
+<div class="top-bar" id="top-bar-menu" data-sticky data-options="marginTop:0;" style="width:100%" data-top-anchor="1" data-btm-anchor="content:bottom">
 	<div class="title-area top-bar-left float-left">
 		<ul class="menu">
 			<li>
@@ -29,3 +55,4 @@
 		</ul>
 	</div>
 </div>
+</div> */ ?>

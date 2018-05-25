c03e7ae7f9707f087f151ed43ebe7790122ea56e
MatiRosero <gekidasa@gmail.com>
2018-05-24 20:26:05 -0600

    shrinks at scroll 96

--- wp-content/themes/castellpons/assets/scripts/cp-sticky-navigation.js
+++ wp-content/themes/castellpons/assets/scripts/cp-sticky-navigation.js
@@ -1,9 +1,18 @@
 jQuery(document).ready(function($) {
 
-	$('#top-bar-menu').on('sticky.zf.stuckto:top', function(){
-	  $(this).addClass('shrink');
-	}).on('sticky.zf.unstuckfrom:top', function(){
-	  $(this).removeClass('shrink');
-	})
+	// $('#top-bar-menu').on('sticky.zf.stuckto:top', function(){
+	//   $(this).addClass('shrink');
+	// }).on('sticky.zf.unstuckfrom:top', function(){
+	//   $(this).removeClass('shrink');
+	// })
+
+	$(window).scroll(function() {
+		var winTop = $(window).scrollTop();
+		if (winTop >= 96) {
+			$("#top-bar-menu").addClass("shrink");
+		} else{
+			$("#top-bar-menu").removeClass("shrink");
+		}
+	});
 
 });

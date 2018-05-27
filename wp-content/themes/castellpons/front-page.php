7631cef10895af2594fa5e5190aaac81305995d3
MatiRosero <gekidasa@gmail.com>
2018-05-27 02:02:57 -0600

    set up orbit

--- wp-content/themes/castellpons/front-page.php
+++ wp-content/themes/castellpons/front-page.php
@@ -12,46 +12,7 @@ get_header(); ?>
 
 		    <main id="main" class="main" role="main">
 
-		    	<?php
-	    		/*
-	    		 * The WordPress Query class.
-	    		 *
-	    		 * @link http://codex.wordpress.org/Function_Reference/WP_Query
-	    		 */
-	    		$args = array(
-
-	    			// Type & Status Parameters
-	    			'post_type'   => 'cp-project',
-	    			'post_status' => 'any',
-
-	    			'post_status' => array(
-	    				'publish',
-	    			),
-
-	    			// Order & Orderby Parameters
-	    			'order'               => 'DESC',
-	    			'orderby'             => 'date',
-
-	    			// Pagination Parameters
-	    			'posts_per_page'         => 3,
-
-	    		);
-
-		    	$query = new WP_Query( $args );
-
-		    	if( $query->have_posts() ):
-				while ( $query->have_posts() ) : $query->the_post();
-
-					echo '<p>';
-					the_title();
-					echo '</p>';
-
-					wp_reset_query();
-
-				endwhile;
-				endif;
-
-		    	?>
+		    	<?php get_template_part( 'parts/content', 'frontpage-slider' ); ?>
 
 		    </main> <!-- end #main -->
 

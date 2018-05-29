4abddf188b49a646adad877ccfbe6ebbf2b2f710
MatiRosero <gekidasa@gmail.com>
2018-05-28 22:42:06 -0600

    footer fixed

--- wp-content/themes/castellpons/footer.php
+++ wp-content/themes/castellpons/footer.php
@@ -10,21 +10,25 @@
 
 		<footer id="site-footer" class="footer" role="contentinfo">
 
-			<div class="inner-footer grid-x grid-padding-x">
+			<div class="inner-footer">
+			<!-- <div class="inner-footer grid-x grid-padding-x"> -->
 
-				<div id="footer-social-menu" class="small-12 medium-4 large-4 cell">
+				<div id="footer-social-menu" class="">
+				<!-- <div id="footer-social-menu" class="small-12 medium-4 large-4 cell"> -->
 					<nav role="navigation">
 						<?php cp_social_media_icons(); ?>
 					</nav>
 				</div>
 
-				<div id="footer-credits" class="small-12 medium-4 large-4 cell">
-					<!-- <p class="source-org copyright">&copy; <?php echo date('Y'); ?> --><?php //bloginfo('name'); ?><strong>Castell + Pons</strong> <?php _e('Architects', 'jointswp'); ?></p>
+				<div id="footer-credits" class="">
+				<!-- <div id="footer-credits" class="small-12 medium-4 large-4 cell"> -->
+					<p class="site-name"><strong>Castell + Pons</strong> <?php _e('Architects', 'jointswp'); ?></p>
 
 				</div>
 
 				<?php if ( is_active_sidebar( 'footer-right' ) ) : ?>
-					<div id="footer-language-menu" class="widget-area small-12 medium-4 large-4 cell" role="complementary">
+					<div id="footer-language-menu" class="widget-area" role="complementary">
+					<!-- <div id="footer-language-menu" class="widget-area small-12 medium-4 large-4 cell" role="complementary"> -->
 						<?php dynamic_sidebar( 'footer-right' ); ?>
 					</div><!-- #primary-sidebar -->
 				<?php endif; ?>

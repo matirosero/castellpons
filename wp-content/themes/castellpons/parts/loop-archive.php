96808e39f3c28182df31eb6563729010c38d1410
MatiRosero <gekidasa@gmail.com>
2018-05-30 01:04:12 -0600

    flex to arrange news grid

--- wp-content/themes/castellpons/parts/loop-archive.php
+++ wp-content/themes/castellpons/parts/loop-archive.php
@@ -12,19 +12,22 @@
 		<?php the_post_thumbnail('news-list-image-full'); ?>
 	</div>
 
-	<div class="news-article-content">
-		<header class="article-header">
-			<h2 class="article-title"><?php the_title(); ?></h2>
-			<?php get_template_part( 'parts/content', 'byline' ); ?>
-		</header> <!-- end article header -->
+	<div class="news-article-inner">
+		<div class="news-article-content">
+			<header class="article-header">
+				<h2 class="article-title"><?php the_title(); ?></h2>
+				<?php //get_template_part( 'parts/content', 'byline' ); ?>
+			</header> <!-- end article header -->
 
-		<section class="entry-content" itemprop="articleBody">
+			<section class="entry-content" itemprop="articleBody">
 
-			<?php the_content('<button class="tiny">' . __( 'Read more...', 'jointswp' ) . '</button>'); ?>
-			<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>
-		</section> <!-- end article section -->
+				<?php the_content('<button class="tiny">' . __( 'Read more...', 'jointswp' ) . '</button>'); ?>
+
+			</section> <!-- end article section -->
+		</div>
 
 		<footer class="article-footer">
+			<p class="tags"><?php the_tags('<span class="tags-title">' . __('Tags:', 'jointstheme') . '</span> ', ', ', ''); ?></p>
 	    	<a href="#" class="button news-article-project-btn"><?php _e( 'See project', 'jointswp' );?></a>
 		</footer> <!-- end article footer -->
 

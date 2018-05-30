0f82a57b423d71486044bb68c0cf2dc84b7fe008
MatiRosero <gekidasa@gmail.com>
2018-05-29 23:43:36 -0600

    sliders fade out

--- wp-content/themes/castellpons/assets/scripts/slider.js
+++ wp-content/themes/castellpons/assets/scripts/slider.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function($) {
         }
         slides[slideIndex-1].style.display = "block";
         bars[slideIndex-1].className += " active";
-        setTimeout(showSlides, 4000); // Change image every 2 seconds
+        setTimeout(showSlides, 5000); // Change image every 2 seconds
     }
 
 });

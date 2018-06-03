b30c21680a6d61ad107cca80217f966abf6a9900
MatiRosero <gekidasa@gmail.com>
2018-06-03 01:33:05 -0600

    slider works with cmb attached project

--- wp-content/themes/castellpons/parts/content-frontpage-slider.php
+++ wp-content/themes/castellpons/parts/content-frontpage-slider.php
@@ -1,90 +1,57 @@
 <?php
 
-echo 'hi';
+
+
 if ( get_post_meta( get_the_ID(), 'mro_cp_frontpage_slider_projects', true ) ) {
 
 	$attached = get_post_meta( get_the_ID(), 'mro_cp_frontpage_slider_projects', true );
 
-	var_dump($attached);
+	// var_dump($attached);
 
-	/*
-	 * The WordPress Query class.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/WP_Query
-	 */
-	$args = array(
+	echo '<div class="slideshow-container">';
 
-		// Type & Status Parameters
-		'post_type'   => 'cp-project',
-		'post_status' => 'any',
+	foreach ($attached as $project_id) {
+		// var_dump($project_id);
+		$attachment_id = get_post_thumbnail_id( $project_id );
+		// var_dump($attachment_id);
+		$url = get_the_post_thumbnail_url( $project_id, 'full' );
+		// var_dump($url);
+		$srcset = wp_get_attachment_image_srcset( $attachment_id, 'full' );
+		// var_dump($srcset);
 
-		'post_status' => array(
-			'publish',
-		),
+		echo '<div class="slides fade">
+			<img src="'.$url.'" srcset="'.$srcset.'" alt="" />
+			</div>';
+	}
 
-		'post__in'	=> $attached,
+	echo '</div><!-- .slideshow-container -->';
 
-		// Order & Orderby Parameters
-		'order'               => 'DESC',
-		'orderby'             => 'date',
+	$count = count($attached);
 
-		// Pagination Parameters
-		'posts_per_page'         => 3,
+	?>
 
-	);
+	<div class="slider-progress" style="text-align:center">
 
-	$query = new WP_Query( $args );
+		<?php
 
-	if( $query->have_posts() ): ?>
+		$i = 0;
 
-		<div class="slideshow-container">
+		while ( $i < $count ) { ?>
 
-			<?php 
-
-			$count = $query->post_count;
-
-			while ( $query->have_posts() ) : $query->the_post(); ?>
-
-				<div class="slides fade">
-
-					<?php
-					the_post_thumbnail('full', ['class' => 'orbit-image', 'title' => 'Feature project']);
-					?>
-
+			<div class="progress-indicator" >
+				<div class="numbertext"><?php echo str_pad($i+1, 2, '0', STR_PAD_LEFT); ?></div>
+				<div class="bar" id="bar-<?php echo $i; ?>">
+					<div class="bar-progress"></div>
 				</div>
+			</div>
 
-				<?php
+			<?php $i++; ?>
 
-				wp_reset_query();
+		<?php } ?>
 
-			endwhile; ?>
+	</div>
 
-		</div><!-- .slideshow-container -->
+	<?php
 
-		<div class="slider-progress" style="text-align:center">
-
-			<?php
-
-			$i = 0;
-
-			while ( $i < $count ) { ?>
-
-				<div class="progress-indicator" >
-					<div class="numbertext"><?php echo str_pad($i+1, 2, '0', STR_PAD_LEFT); ?></div>
-					<div class="bar" id="bar-<?php echo $i; ?>">
-						<div class="bar-progress"></div>
-					</div>
-				</div>
-
-				<?php $i++; ?>
-
-			<?php } ?>
-
-		</div>
-
-	<?php endif; 
-
-} else {
-	echo 'no';
 }
 
